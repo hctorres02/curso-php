e7d441f738fc1c6255d04d4cd260420e43cdf0a6
HC Torres <hctorres02@gmail.com>
2025-03-16 15:17:34 -0300

    Autenticação: middlewares

---
+++ src/Middlewares/Visitante.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Middlewares;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class Visitante
+{
+    public function __invoke(): bool|RedirectResponse
+    {
+        if (session()->get('usuario')) {
+            return redirect('/usuarios/editar');
+        }
+
+        return true;
+    }
+}
--- src/routes.php
+++ src/routes.php
@@ -7,51 +7,53 @@ use App\Controllers\DisciplinaController;
 use App\Controllers\PeriodoController;
 use App\Controllers\UsuarioController;
 use App\Http\Router;
+use App\Middlewares\AcessoRestrito;
+use App\Middlewares\Visitante;
 
 // página inicial
 Router::redirect('/', '/agendamentos');
 
 // autenticação
-Router::get('/login', [AuthController::class, 'index']);
-Router::post('/login', [AuthController::class, 'login']);
+Router::get('/login', [AuthController::class, 'index'], [Visitante::class]);
+Router::post('/login', [AuthController::class, 'login'], [Visitante::class]);
 Router::get('/logout', [AuthController::class, 'logout']);
 
 // usuários
-Router::get('/usuarios', [UsuarioController::class, 'index']);
-Router::get('/usuarios/cadastrar', [UsuarioController::class, 'cadastrar']);
-Router::post('/usuarios', [UsuarioController::class, 'salvar']);
-Router::get('/usuarios/editar', [UsuarioController::class, 'editar']);
-Router::put('/usuarios/editar', [UsuarioController::class, 'atualizar']);
+Router::get('/usuarios', [UsuarioController::class, 'index'], [AcessoRestrito::class]);
+Router::get('/usuarios/cadastrar', [UsuarioController::class, 'cadastrar'], [Visitante::class]);
+Router::post('/usuarios', [UsuarioController::class, 'salvar'], [Visitante::class]);
+Router::get('/usuarios/editar', [UsuarioController::class, 'editar'], [AcessoRestrito::class]);
+Router::put('/usuarios/editar', [UsuarioController::class, 'atualizar'], [AcessoRestrito::class]);
 
 // agendamentos
 Router::get('/agendamentos', [AgendamentoController::class, 'index']);
-Router::get('/agendamentos/cadastrar', [AgendamentoController::class, 'cadastrar']);
-Router::post('/agendamentos', [AgendamentoController::class, 'salvar']);
-Router::get('/agendamentos/{agendamento}', [AgendamentoController::class, 'ver']);
-Router::get('/agendamentos/{periodo}/editar', [AgendamentoController::class, 'editar']);
-Router::put('/agendamentos/{periodo}', [AgendamentoController::class, 'atualizar']);
-Router::delete('/agendamentos/{periodo}', [AgendamentoController::class, 'excluir']);
+Router::get('/agendamentos/cadastrar', [AgendamentoController::class, 'cadastrar'], [AcessoRestrito::class]);
+Router::post('/agendamentos', [AgendamentoController::class, 'salvar'], [AcessoRestrito::class]);
+Router::get('/agendamentos/{agendamento}', [AgendamentoController::class, 'ver'], [AcessoRestrito::class]);
+Router::get('/agendamentos/{periodo}/editar', [AgendamentoController::class, 'editar'], [AcessoRestrito::class]);
+Router::put('/agendamentos/{periodo}', [AgendamentoController::class, 'atualizar'], [AcessoRestrito::class]);
+Router::delete('/agendamentos/{periodo}', [AgendamentoController::class, 'excluir'], [AcessoRestrito::class]);
 
 // períodos
-Router::get('/periodos', [PeriodoController::class, 'index']);
-Router::get('/periodos/cadastrar', [PeriodoController::class, 'cadastrar']);
-Router::post('/periodos', [PeriodoController::class, 'salvar']);
-Router::get('/periodos/{periodo}/editar', [PeriodoController::class, 'editar']);
-Router::put('/periodos/{periodo}', [PeriodoController::class, 'atualizar']);
-Router::delete('/periodos/{periodo}', [PeriodoController::class, 'excluir']);
+Router::get('/periodos', [PeriodoController::class, 'index'], [AcessoRestrito::class]);
+Router::get('/periodos/cadastrar', [PeriodoController::class, 'cadastrar'], [AcessoRestrito::class]);
+Router::post('/periodos', [PeriodoController::class, 'salvar'], [AcessoRestrito::class]);
+Router::get('/periodos/{periodo}/editar', [PeriodoController::class, 'editar'], [AcessoRestrito::class]);
+Router::put('/periodos/{periodo}', [PeriodoController::class, 'atualizar'], [AcessoRestrito::class]);
+Router::delete('/periodos/{periodo}', [PeriodoController::class, 'excluir'], [AcessoRestrito::class]);
 
 // disciplinas
-Router::get('/disciplinas', [DisciplinaController::class, 'index']);
-Router::get('/disciplinas/cadastrar', [DisciplinaController::class, 'cadastrar']);
-Router::post('/disciplinas', [DisciplinaController::class, 'salvar']);
-Router::get('/disciplinas/{atividade}/editar', [DisciplinaController::class, 'editar']);
-Router::put('/disciplinas/{atividade}', [DisciplinaController::class, 'atualizar']);
-Router::delete('/disciplinas/{atividade}', [DisciplinaController::class, 'excluir']);
+Router::get('/disciplinas', [DisciplinaController::class, 'index'], [AcessoRestrito::class]);
+Router::get('/disciplinas/cadastrar', [DisciplinaController::class, 'cadastrar'], [AcessoRestrito::class]);
+Router::post('/disciplinas', [DisciplinaController::class, 'salvar'], [AcessoRestrito::class]);
+Router::get('/disciplinas/{atividade}/editar', [DisciplinaController::class, 'editar'], [AcessoRestrito::class]);
+Router::put('/disciplinas/{atividade}', [DisciplinaController::class, 'atualizar'], [AcessoRestrito::class]);
+Router::delete('/disciplinas/{atividade}', [DisciplinaController::class, 'excluir'], [AcessoRestrito::class]);
 
 // atividades
-Router::get('/atividades', [AtividadeController::class, 'index']);
-Router::get('/atividades/cadastrar', [AtividadeController::class, 'cadastrar']);
-Router::post('/atividades', [AtividadeController::class, 'salvar']);
-Router::get('/atividades/{atividade}/editar', [AtividadeController::class, 'editar']);
-Router::put('/atividades/{atividade}', [AtividadeController::class, 'atualizar']);
-Router::delete('/atividades/{atividade}', [AtividadeController::class, 'excluir']);
+Router::get('/atividades', [AtividadeController::class, 'index'], [AcessoRestrito::class]);
+Router::get('/atividades/cadastrar', [AtividadeController::class, 'cadastrar'], [AcessoRestrito::class]);
+Router::post('/atividades', [AtividadeController::class, 'salvar'], [AcessoRestrito::class]);
+Router::get('/atividades/{atividade}/editar', [AtividadeController::class, 'editar'], [AcessoRestrito::class]);
+Router::put('/atividades/{atividade}', [AtividadeController::class, 'atualizar'], [AcessoRestrito::class]);
+Router::delete('/atividades/{atividade}', [AtividadeController::class, 'excluir'], [AcessoRestrito::class]);
