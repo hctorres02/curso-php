4014c7a08a3a224edb8a4bc63c2e40c750dbdb5d
HC Torres <hctorres02@gmail.com>
2025-03-23 15:31:02 -0300

    Aprimora routes.php

--- src/routes.php
+++ src/routes.php
@@ -17,52 +17,330 @@ use App\Middlewares\Visitante;
 Router::redirect('/', '/agendamentos');
 
 // autenticação
-Router::get('/login', [AuthController::class, 'index'], [Visitante::class]);
-Router::post('/login', [AuthController::class, 'login'], [Visitante::class]);
-Router::get('/logout', [AuthController::class, 'logout']);
+Router::get(
+    uri: '/login',
+    action: [AuthController::class, 'index'],
+    middlewares: [
+        Visitante::class,
+    ]
+);
+
+Router::post(
+    uri: '/login',
+    action: [AuthController::class, 'login'],
+    middlewares: [
+        Visitante::class,
+    ]
+);
+
+Router::get(
+    uri: '/logout',
+    action: [AuthController::class, 'logout']
+);
 
 // cadastro
-Router::get('/cadastro', [CadastroController::class, 'cadastrar'], [Visitante::class]);
-Router::post('/cadastro', [CadastroController::class, 'salvar'], [Visitante::class]);
-Router::get('/cadastro/editar', [CadastroController::class, 'editar'], [AcessoRestrito::class, Permission::MANTER_PERFIL]);
-Router::put('/cadastro/editar', [CadastroController::class, 'atualizar'], [AcessoRestrito::class, Permission::MANTER_PERFIL]);
+Router::get(
+    uri: '/cadastro',
+    action: [CadastroController::class, 'cadastrar'],
+    middlewares: [
+        Visitante::class,
+    ]
+);
+
+Router::post(
+    uri: '/cadastro',
+    action: [CadastroController::class, 'salvar'],
+    middlewares: [
+        Visitante::class,
+    ]
+);
+
+Router::get(
+    uri: '/cadastro/editar',
+    action: [CadastroController::class, 'editar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_PERFIL,
+    ]
+);
+
+Router::put(
+    uri: '/cadastro/editar',
+    action: [CadastroController::class, 'atualizar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_PERFIL,
+    ]
+);
 
 // usuários
-Router::get('/usuarios', [UsuarioController::class, 'index'], [AcessoRestrito::class, Permission::MANTER_USUARIOS]);
-Router::get('/usuarios/cadastrar', [UsuarioController::class, 'cadastrar'], [AcessoRestrito::class, Permission::MANTER_USUARIOS]);
-Router::post('/usuarios', [UsuarioController::class, 'salvar'], [AcessoRestrito::class, Permission::MANTER_USUARIOS]);
-Router::get('/usuarios/{usuario}/editar', [UsuarioController::class, 'editar'], [AcessoRestrito::class, Permission::MANTER_USUARIOS, NaoPodeEditarAdminOuSiMesmo::class]);
-Router::put('/usuarios/{usuario}', [UsuarioController::class, 'atualizar'], [AcessoRestrito::class, Permission::MANTER_USUARIOS, NaoPodeEditarAdminOuSiMesmo::class]);
+Router::get(
+    uri: '/usuarios',
+    action: [UsuarioController::class, 'index'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_USUARIOS,
+    ]
+);
 
-// agendamentos
-Router::get('/agendamentos', [AgendamentoController::class, 'index']);
-Router::get('/agendamentos/cadastrar', [AgendamentoController::class, 'cadastrar'], [AcessoRestrito::class, Permission::MANTER_AGENDAMENTOS]);
-Router::post('/agendamentos', [AgendamentoController::class, 'salvar'], [AcessoRestrito::class, Permission::MANTER_AGENDAMENTOS]);
-Router::get('/agendamentos/{agendamento}', [AgendamentoController::class, 'ver'], [AcessoRestrito::class, Permission::MANTER_AGENDAMENTOS]);
-Router::get('/agendamentos/{periodo}/editar', [AgendamentoController::class, 'editar'], [AcessoRestrito::class, Permission::MANTER_AGENDAMENTOS]);
-Router::put('/agendamentos/{periodo}', [AgendamentoController::class, 'atualizar'], [AcessoRestrito::class, Permission::MANTER_AGENDAMENTOS]);
-Router::delete('/agendamentos/{periodo}', [AgendamentoController::class, 'excluir'], [AcessoRestrito::class, Permission::MANTER_AGENDAMENTOS]);
+Router::get(
+    uri: '/usuarios/cadastrar',
+    action: [UsuarioController::class, 'cadastrar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_USUARIOS,
+    ]
+);
+
+Router::post(
+    uri: '/usuarios',
+    action: [UsuarioController::class, 'salvar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_USUARIOS,
+    ]
+);
+
+Router::get(
+    uri: '/usuarios/{usuario}/editar',
+    action: [UsuarioController::class, 'editar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_USUARIOS,
+        NaoPodeEditarAdminOuSiMesmo::class,
+    ]
+);
+
+Router::put(
+    uri: '/usuarios/{usuario}',
+    action: [UsuarioController::class, 'atualizar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_USUARIOS,
+        NaoPodeEditarAdminOuSiMesmo::class,
+    ]
+);
+
+Router::get(
+    uri: '/agendamentos',
+    action: [AgendamentoController::class, 'index']
+);
+
+Router::get(
+    uri: '/agendamentos/cadastrar',
+    action: [AgendamentoController::class, 'cadastrar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_AGENDAMENTOS,
+    ]
+);
+
+Router::post(
+    uri: '/agendamentos',
+    action: [AgendamentoController::class, 'salvar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_AGENDAMENTOS,
+    ]
+);
+
+Router::get(
+    uri: '/agendamentos/{agendamento}',
+    action: [AgendamentoController::class, 'ver'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_AGENDAMENTOS,
+    ]
+);
+
+Router::get(
+    uri: '/agendamentos/{agendamento}/editar',
+    action: [AgendamentoController::class, 'editar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_AGENDAMENTOS,
+    ]
+);
+
+Router::put(
+    uri: '/agendamentos/{agendamento}',
+    action: [AgendamentoController::class, 'atualizar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_AGENDAMENTOS,
+    ]
+);
+
+Router::delete(
+    uri: '/agendamentos/{agendamento}',
+    action: [AgendamentoController::class, 'excluir'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_AGENDAMENTOS,
+    ]
+);
 
 // períodos
-Router::get('/periodos', [PeriodoController::class, 'index'], [AcessoRestrito::class, Permission::MANTER_PERIODOS]);
-Router::get('/periodos/cadastrar', [PeriodoController::class, 'cadastrar'], [AcessoRestrito::class, Permission::MANTER_PERIODOS]);
-Router::post('/periodos', [PeriodoController::class, 'salvar'], [AcessoRestrito::class, Permission::MANTER_PERIODOS]);
-Router::get('/periodos/{periodo}/editar', [PeriodoController::class, 'editar'], [AcessoRestrito::class, Permission::MANTER_PERIODOS]);
-Router::put('/periodos/{periodo}', [PeriodoController::class, 'atualizar'], [AcessoRestrito::class, Permission::MANTER_PERIODOS]);
-Router::delete('/periodos/{periodo}', [PeriodoController::class, 'excluir'], [AcessoRestrito::class, Permission::MANTER_PERIODOS]);
+Router::get(
+    uri: '/periodos',
+    action: [PeriodoController::class, 'index'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_PERIODOS,
+    ]
+);
+
+Router::get(
+    uri: '/periodos/cadastrar',
+    action: [PeriodoController::class, 'cadastrar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_PERIODOS,
+    ]
+);
+
+Router::post(
+    uri: '/periodos',
+    action: [PeriodoController::class, 'salvar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_PERIODOS,
+    ]
+);
+
+Router::get(
+    uri: '/periodos/{periodo}/editar',
+    action: [PeriodoController::class, 'editar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_PERIODOS,
+    ]
+);
+
+Router::put(
+    uri: '/periodos/{periodo}',
+    action: [PeriodoController::class, 'atualizar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_PERIODOS,
+    ]
+);
+
+Router::delete(
+    uri: '/periodos/{periodo}',
+    action: [PeriodoController::class, 'excluir'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_PERIODOS,
+    ]
+);
 
 // disciplinas
-Router::get('/disciplinas', [DisciplinaController::class, 'index'], [AcessoRestrito::class, Permission::MANTER_DISCIPLINAS]);
-Router::get('/disciplinas/cadastrar', [DisciplinaController::class, 'cadastrar'], [AcessoRestrito::class, Permission::MANTER_DISCIPLINAS]);
-Router::post('/disciplinas', [DisciplinaController::class, 'salvar'], [AcessoRestrito::class, Permission::MANTER_DISCIPLINAS]);
-Router::get('/disciplinas/{atividade}/editar', [DisciplinaController::class, 'editar'], [AcessoRestrito::class, Permission::MANTER_DISCIPLINAS]);
-Router::put('/disciplinas/{atividade}', [DisciplinaController::class, 'atualizar'], [AcessoRestrito::class, Permission::MANTER_DISCIPLINAS]);
-Router::delete('/disciplinas/{atividade}', [DisciplinaController::class, 'excluir'], [AcessoRestrito::class, Permission::MANTER_DISCIPLINAS]);
+Router::get(
+    uri: '/disciplinas',
+    action: [DisciplinaController::class, 'index'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_DISCIPLINAS,
+    ]
+);
+
+Router::get(
+    uri: '/disciplinas/cadastrar',
+    action: [DisciplinaController::class, 'cadastrar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_DISCIPLINAS,
+    ]
+);
+
+Router::post(
+    uri: '/disciplinas',
+    action: [DisciplinaController::class, 'salvar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_DISCIPLINAS,
+    ]
+);
+
+Router::get(
+    uri: '/disciplinas/{disciplina}/editar',
+    action: [DisciplinaController::class, 'editar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_DISCIPLINAS,
+    ]
+);
+
+Router::put(
+    uri: '/disciplinas/{disciplina}',
+    action: [DisciplinaController::class, 'atualizar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_DISCIPLINAS,
+    ]
+);
+
+Router::delete(
+    uri: '/disciplinas/{disciplina}',
+    action: [DisciplinaController::class, 'excluir'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_DISCIPLINAS,
+    ]
+);
 
 // atividades
-Router::get('/atividades', [AtividadeController::class, 'index'], [AcessoRestrito::class, Permission::MANTER_ATIVIDADES]);
-Router::get('/atividades/cadastrar', [AtividadeController::class, 'cadastrar'], [AcessoRestrito::class, Permission::MANTER_ATIVIDADES]);
-Router::post('/atividades', [AtividadeController::class, 'salvar'], [AcessoRestrito::class, Permission::MANTER_ATIVIDADES]);
-Router::get('/atividades/{atividade}/editar', [AtividadeController::class, 'editar'], [AcessoRestrito::class, Permission::MANTER_ATIVIDADES]);
-Router::put('/atividades/{atividade}', [AtividadeController::class, 'atualizar'], [AcessoRestrito::class, Permission::MANTER_ATIVIDADES]);
-Router::delete('/atividades/{atividade}', [AtividadeController::class, 'excluir'], [AcessoRestrito::class, Permission::MANTER_ATIVIDADES]);
+Router::get(
+    uri: '/atividades',
+    action: [AtividadeController::class, 'index'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_ATIVIDADES,
+    ]
+);
+
+Router::get(
+    uri: '/atividades/cadastrar',
+    action: [AtividadeController::class, 'cadastrar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_ATIVIDADES,
+    ]
+);
+
+Router::post(
+    uri: '/atividades',
+    action: [AtividadeController::class, 'salvar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_ATIVIDADES,
+    ]
+);
+
+Router::get(
+    uri: '/atividades/{atividade}/editar',
+    action: [AtividadeController::class, 'editar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_ATIVIDADES,
+    ]
+);
+
+Router::put(
+    uri: '/atividades/{atividade}',
+    action: [AtividadeController::class, 'atualizar'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_ATIVIDADES,
+    ]
+);
+
+Router::delete(
+    uri: '/atividades/{atividade}',
+    action: [AtividadeController::class, 'excluir'],
+    middlewares: [
+        AcessoRestrito::class,
+        Permission::MANTER_ATIVIDADES,
+    ]
+);
