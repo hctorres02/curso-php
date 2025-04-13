c3cf0428af11264d06b7f21cff28441e3ee85e0c
HC Torres <hctorres02@gmail.com>
2025-04-13 13:31:40 -0300

    Aprimora middlewares AcessoRestrito e Visitante: autenticação automatizada

--- src/Middlewares/AcessoRestrito.php
+++ src/Middlewares/AcessoRestrito.php
@@ -3,12 +3,20 @@
 namespace App\Middlewares;
 
 use App\Http\Request;
+use App\Models\Usuario;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class AcessoRestrito
 {
     public function __invoke(Request $request) : bool|RedirectResponse
     {
+        if ($request->get('_app_key') === env('APP_KEY')) {
+            $admin = Usuario::firstWhere('email', env('ADMIN_EMAIL'));
+
+            session()->set('usuario_id', $admin->id);
+            session()->set('usuario', $admin);
+        }
+
         if (session()->get('usuario')) {
             return true;
         }
--- src/Middlewares/Visitante.php
+++ src/Middlewares/Visitante.php
@@ -2,12 +2,21 @@
 
 namespace App\Middlewares;
 
+use App\Http\Request;
+use App\Models\Usuario;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class Visitante
 {
-    public function __invoke(): bool|RedirectResponse
+    public function __invoke(Request $request): bool|RedirectResponse
     {
+        if ($request->get('_app_key') === env('APP_KEY')) {
+            $admin = Usuario::firstWhere('email', env('ADMIN_EMAIL'));
+
+            session()->set('usuario_id', $admin->id);
+            session()->set('usuario', $admin);
+        }
+
         if (session()->get('usuario')) {
             return redirectRoute('editar_cadastro');
         }
